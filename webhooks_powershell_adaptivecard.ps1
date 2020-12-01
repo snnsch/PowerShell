ab8a1881ae906e50bea57888d80dce2b0ebdb3fd
snnsch <40392901+snnsch@users.noreply.github.com>
2020-12-01 10:14:41 +0100

    Update webhooks_powershell_adaptivecard.ps1

--- webhooks_powershell_adaptivecard.ps1
+++ webhooks_powershell_adaptivecard.ps1
@@ -1,7 +1,7 @@
 # Link to Teams Sharepoint site
-$adminURL ="https://m365x425026-admin.sharepoint.com"
+$adminURL = <YOUR URL>
 # Teams connectors - webhook URI
-$connectorUri = 'https://outlook.office.com/webhook/8aba3d8f-7014-469e-96be-a3a8bcc25ef6@544bc022-09a1-4846-ab3c-d329dad23be2/IncomingWebhook/8b717eb61b50439da88446b123d05786/3422ada9-b5a0-4ef0-958d-a02ee3f9c0b9'
+$connectorUri = <YOUR WEBHOOK URI>
 
 # Install-Module SharePointPnPPowerShellOnline
 Connect-PnPOnline –Url $adminURL –Credentials (Get-Credential)
