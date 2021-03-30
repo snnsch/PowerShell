1985d42869ee8e2f0747178112f3a019fd3627b5
Anna Weninger <aennchen@gmx.net>
2021-03-30 12:57:19 +0200

    v2 for GraphSDK send message to channel for blog

--- Graph API/graph-api-sdk-channelMsg-HTML.ps1
+++ Graph API/graph-api-sdk-channelMsg-HTML.ps1
@@ -1,28 +1,46 @@
+# NOTES FOR BLOG ARTICLES:
+# a) V1 of this script (retrieve via GitHub) is very useful as a first article, V2 (30 Mar) as second one
+# Concept for articles: (a) connecting to GraphAPI via PowerShell SDK, (b) debugging values using Graph Explorer, (c) posting for a fixed user, (d) Team and channel without mention, (5) posting with random team and channel, (6) creating mention, (7) styling output
+
+
 # NOTES: 
     # Uses PowerShell SDK "MgGraph" for Graph API calls and request handling; 
     # Very cool but not production-ready yet => follow updates in documentation, e.g.: https://github.com/microsoftgraph/msgraph-sdk-powershell/tree/dev/samples
 
 # (1) Set permissions for following actions => this would normally need to be done in Azure => VERY CONVENIENT (check needed permissions in Graph Explorer)
-Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
+# Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
 
 # (2) Get specific user and id
-$user = Get-MgUser -Filter "DisplayName eq '<DISPLAY_NAME>'"
-#Write-Output("Teamuser ID:",$user.id)
-Write-Output("Displayname:",$user.DisplayName)
+$ENTER_USER = 'anna-admin@M365x425026.onmicrosoft.com'
+$user = Get-MgUser -Filter "mail eq '$ENTER_USER'"
+Write-Output("Chosen User ID: $($user.id)")
+Write-Output("Displayname: $($user.DisplayName)")
 
 # (3) Get Team to post in
-$team = Get-MgUserJoinedTeam -UserId $user.id -Filter "DisplayName eq '<TEAM_DISPLAY_NAME>'"
-#Write-Output("Team-ID:",$team.id)
+# $team = Get-MgUserJoinedTeam -UserId $user.id -Filter "DisplayName eq 'aweninger-outlook'" ///OLD
+function Get-Random-Team-ID {
+    $all_joined_teams = Get-MgUserJoinedTeam -UserId $user.id
+    $first_random_team = $all_joined_teams | Select-object -Property id -First 1
+    Write-Output "Random Team ID: $($first_random_team.id)"
+    return $first_random_team.id
+}
+
+# call function, sadly returns weird array where content is only in [1] so need to access that
+$teamid_array = Get-Random-Team-ID
+$teamid = $teamid_array[1]
+Write-Output "Chosen Team ID: $teamid"
 
 # (4) Get channel to post in
-$channel = Get-MgTeamChannel -TeamId $team.id -Filter "DisplayName eq '<CHANNEL_DISPLAY_NAME>'"
-#Write-Output("Channel-ID:",$channel.id)
+$all_channels = Get-MgTeamChannel -TeamId $teamid
+$first_random_channel = $all_channels | Select-object -Property id -First 1
+Write-Output("Channel-ID: $($first_random_channel.id)")
+$channelid = $first_random_channel.id
 
 # (5) Create user mention item for channel mentioning
 $UserToMention  = @{
     Id = $User.Id;
     DisplayName = $User.DisplayName;
-    #UserIdentityType = "aadUser"; #optional
+    # UserIdentityType = "aadUser"; #optional
 }
 
 # Compose BASIC Teams channel message
@@ -30,11 +48,15 @@ $UserToMention  = @{
 
 # Compose ADVANCED message - HTML with style + mention Teams message (surround with '') => #TODO: DOES NOT show in "activity"
 
-New-MgTeamChannelMessage -ChannelId $channel.id -TeamId $team.Id `
+# Enter colour name for bg color of message
+$bgcolors = @('Aqua','Aquamarine','Chartreuse', 'DeepPink', 'Gold', 'Indigo', 'SlateBlue')
+$randomcolor = $bgcolors | Get-Random
+
+New-MgTeamChannelMessage -ChannelId $channelid -TeamId $teamid `
     -Body @{ `
         ContentType = 'html'; `
         Encoding = 'UTF8'; ` #TODO: added this manually to fix UTF8 encoding, doesn't work, umlauts and this 🤣 won't display properly
-        Content = 'Hello there! <h1 style="background-color:powderblue;"> Hell yeah, well done <at id="1">'+ $user.DisplayName +'</at> !!! </h1>' `
+        Content = '<h1> WELL DONE, YOU! </h1> </br> <p style="padding:10px; font-weight:bold; background-color:'+$randomcolor+'"> Successfully posted in Teams, <at id="1">'+ $user.DisplayName +'</at> ! </p>' `
     } `
     -Mentions @( `
         @{ `
